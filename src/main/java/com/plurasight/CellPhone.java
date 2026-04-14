2321dbba41f7b6c9aded82db68f85b27e36721fc
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-14 15:00:10 -0400

    Created Getter and setter

--- src/main/java/com/plurasight/CellPhone.java
+++ src/main/java/com/plurasight/CellPhone.java
@@ -1,4 +1,58 @@
 package com.plurasight;
 
 public class CellPhone {
+    //Properties
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+
+    // Constructor
+    public CellPhone(){
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+
+
+        }
+    //Getter and Setter
+    //Serial Number
+    public int getSerialNumber(){
+        return serialNumber;
+    }
+    public void setSerialNumber(int serialNumber){
+        this.serialNumber = serialNumber;
+    }
+    //Model
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    //Carrier
+    public String getCarrier(){
+        return carrier;
+    }
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+    //Phone Number
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    //Owner
+    public String getOwner(){
+        return owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
--- src/main/java/com/plurasight/CellPhoneApplication.java
+++ src/main/java/com/plurasight/CellPhoneApplication.java
@@ -1,4 +1,6 @@
 package com.plurasight;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+    }
 }
